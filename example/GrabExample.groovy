f547242640481d439841c3d656173c664db9f333
Kazuki Minamitani <ka.minami@nifty.com>
2017-02-16 14:35:11 +0900

    Grabを用いたスクリプト例を追加

---
+++ example/GrabExample.groovy
@@ -0,0 +1,13 @@
+@GrabResolver(name="gexcelparser", root="https://github.com/sorabito/gexcelparser/raw/master/repository/")
+@Grab("com.sorabito:gexcelparser:1.0.0")
+
+import com.sorabito.gexcelparser.GExcelParser
+import com.sorabito.gexcelparser.PropertyMapper
+
+def parser = GExcelParser.open('example.xlsx')
+List<PropertyMapper> mapperList = parser.parseSheet('Sheet1')
+
+mapperList.each { mapper ->
+    println "${mapper.column1}: ${mapper.column2}"
+}
+
